45ba463db0a671db0ae25da981b1b8c5b7b650c7
nicktu12 <nickteets@gmail.com>
2019-04-02 16:37:15 -0600

    Add current and favorites section

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
   <v-app>
     <v-content>
       <Search :updateWeatherData="updateWeatherData" />
-      <Current />
+      <Favorites />
+      <Current 
+        v-if="Object.keys(weatherData).length > 0"
+        :value="dailyTemperatures"
+        :dates="dates"
+      />
     </v-content>
   </v-app>
 </template>
@@ -24,7 +29,15 @@ export default {
   },
   methods: {
     updateWeatherData: function(data) {
-      console.log({data})
+      this.weatherData = data;
+    }
+  },
+  computed: {
+    dailyTemperatures: function() {
+      return this.weatherData.consolidated_weather.map(day => parseInt(day.the_temp.toFixed(2)));
+    },
+    dates: function() {
+      return this.weatherData.consolidated_weather.map(day => day.applicable_date);
     }
   }
 }
--- src/components/Current.vue
+++ src/components/Current.vue
@@ -1,63 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="current">
+    <v-sparkline
+        :fill="true"
+        :gradient="['red', 'orange', 'yellow']"
+        :line-width="2"
+        :padding="0"
+        :smooth="6 || false"
+        :value="value"
+        auto-draw
+      ></v-sparkline>
+      <v-bottom-nav>
+        <span v-for="(date, index) in dates" style="text-align: center;" :key="index">
+          {{date}}
+        </span>
+      </v-bottom-nav>
+      <span class="max">{{high}} °F</span>
+      <span class="min">{{low}} °F</span>
+      <v-btn
+        absolute
+        dark
+        fab
+        top
+        right
+        color="#02A9EA"
+      ><v-icon style="font-style: normal;">☆</v-icon></v-btn>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Current',
   props: {
-    msg: String
+    value: Array,
+    dates: Array
   },
-  mounted: function() {
-    fetch('https://api.codetabs.com/v1/proxy?quest=https://www.metaweather.com/api/location/44418/')
-      .then(res => res.json())
-      .then(res => { console.log(res) })
+  computed: {
+    low: function() {
+      return Math.min(...this.value) * 9 / 5 + 32;
+    },
+    high: function() {
+      return Math.max(...this.value) * 9 / 5 + 32;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .current {
+    position: relative;
+  }
+
+  .current .max {
+    position: absolute;
+    top: 75px;
+    left: 10px;
+    font-size: 16px;
+  }
+  
+  .current .min {
+    position: absolute;
+    bottom: 75px;
+    left: 10px;
+    font-size: 16px;
+  }
 </style>
